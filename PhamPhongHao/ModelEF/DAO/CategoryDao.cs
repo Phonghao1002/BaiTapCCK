3bae45f11192a5a61ddf645d854a4f20af1131f0
Phong Hao <phamphonghao1002@gmail.com>
2021-06-20 22:53:47 +0700

    a

---
+++ PhamPhongHao/ModelEF/DAO/CategoryDao.cs
@@ -0,0 +1,23 @@
+﻿using ModelEF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelEF.DAO
+{
+    public class CategoryDao
+    {
+        private PhamPhongHaoContext db;
+        public CategoryDao()
+        {
+            db = new PhamPhongHaoContext();
+        }
+        public List<Category> ListAll()
+        {
+            return db.Categories.ToList();
+        }
+
+    }
+}
---
+++ PhamPhongHao/ModelEF/Model/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace ModelEF.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        [StringLength(50)]
+        public string ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Tên")]
+        public string Name { get; set; }
+        [Display(Name = "Giá")]
+        public int? UnitCost { get; set; }
+        [Display(Name = "Số luọng còn")]
+
+        public int? Quantity { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Loại sản phẩm")]
+        public string CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }//khoÁ ngoại 
+    }
+}
